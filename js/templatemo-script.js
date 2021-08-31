343b4b63414c592671276f3ff1bef63db66406d0
first-steps-web <78705415+first-steps-web@users.noreply.github.com>
2021-08-31 12:39:34 +0300

    Add files via upload

---
+++ js/templatemo-script.js
@@ -0,0 +1,95 @@
+function adjustMainBgHeight() {
+    var imgWidth = 1200;
+    var imgHeight = 620;
+    var img = $('.tm-main-bg');
+    var currentWidth = img.width();
+    var currentHeight = (currentWidth * imgHeight) / imgWidth;
+    
+    img.css('height', currentHeight);
+}
+
+$(document).ready(function(){
+    adjustMainBgHeight();
+    
+    $(window).resize(function() {
+        adjustMainBgHeight();
+    });
+    
+    // Mobile menu
+    $('.tm-mobile-menu').click(function() {
+      $('.tm-nav').toggleClass('show');
+    });
+    
+    if($('.tm-carousel').length) {
+      // Carousel
+      $('.tm-carousel').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 420,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });   
+    }  
+});// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
